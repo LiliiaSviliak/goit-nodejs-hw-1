e2d7c04ef79969d62537013e6967fdcc50cbe37f
Liliia Sviliak <66726289+LiliiaSviliak@users.noreply.github.com>
2021-08-16 02:08:14 +0300

    finished

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # goit-nodejs-hw-1
+
+Отримуємо і виводимо список контактів у вигляді таблиці
+<!-- node index.js --action="list" -->
+node index.js --action list
+
+Отримуємо контакт (id)
+<!-- node index.js --action="get" --id=5 -->
+
+Додаємо контакт
+<!-- node index.js --action="add" --name="Mango" --email="mango@gmail.com" --phone="322-22-22" -->
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+Видаляємо контакт
+<!-- node index.js --action="remove" --id=3 -->
+node index.js --action remove --id=3
+
+Результат виконання команд
+list: http://joxi.net/823lZgjtaKdvPA
+get: http://joxi.net/8AnRv30Co0e3ZA
+add: http://joxi.net/p27NkgLinEXBom
+remove: http://joxi.net/zANP8Q5H8xWD6m
---
+++ contacts.js
@@ -0,0 +1,83 @@
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+const utFormate = "utf-8";
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+    fs.readFile(contactsPath, utFormate, (err, data) => {
+        try {
+            const contacts = JSON.parse(data);
+            console.table(contacts);
+        } catch (err) {
+            console.log("listContacts error", err);
+        }
+    });
+}
+
+function getContactById(contactId) {
+    fs.readFile(contactsPath, utFormate, (err, data) => {
+        try {
+            const contacts = JSON.parse(data);
+            const contactById = contacts.find(
+                (item) => String(item.id) === String(contactId)
+            );
+
+            console.table(contactById);
+        } catch (err) {
+            console.log("getContactById error", err);
+        }
+    });
+}
+function removeContact(contactId) {
+    fs.readFile(contactsPath, utFormate, (err, data) => {
+        try {
+            const contacts = JSON.parse(data);
+            const newContacts = contacts.filter(
+                (item) => String(item.id) !== String(contactId)
+            );
+            console.table(newContacts);
+            writeNewContacts(contactsPath, newContacts);
+        } catch (err) {
+            console.log("getContactById error", err);
+        }
+    });
+}
+
+function addContact(name, email, phone) {
+    fs.readFile(contactsPath, utFormate, (err, data) => {
+        try {
+            const contacts = JSON.parse(data);
+            const newContact = {
+                id: uuidv4(),
+                name,
+                email,
+                phone,
+            };
+            const newContacts = [...contacts, newContact];
+            console.table(newContacts);
+            writeNewContacts(contactsPath, newContacts);
+        } catch (err) {
+            console.log("getContactById error", err);
+        }
+    });
+}
+
+function writeNewContacts(path, contacts) {
+    const newJSONContacts = JSON.stringify(contacts);
+    fs.writeFile(path, newJSONContacts, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+}
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+};
